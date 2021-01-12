c1dd9835fa318f5483223a47ce3e62efba3546ab
Nick Scala <nick.scala@me.com>
2021-01-12 10:32:11 -0500

    Updating PHP file for contact form testing

--- php/contact_form.php
+++ php/contact_form.php
@@ -1,7 +1,7 @@
 <?php
 
 // configure
-$from = 'nick.scala@me.com'; // Replace it with Your Hosting Admin email. REQUIRED!
+$from = 'info@nickscala.com'; // Replace it with Your Hosting Admin email. REQUIRED!
 $sendTo = 'nick.scala@me.com'; // Replace it with Your email. REQUIRED!
 $subject = 'New message from contact form';
 $fields = array('name' => 'Name', 'email' => 'Email', 'subject' => 'Subject', 'message' => 'Message'); // array variable name => Text to appear in the email. If you added or deleted a field in the contact form, edit this array.
